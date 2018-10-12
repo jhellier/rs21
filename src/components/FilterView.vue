2ead30540a53637970f282883683f5a44cf7c4be
John Hellier <john@Johns-MacBook-Pro-2.local>
2018-10-12 15:09:59 -0600

    Addded new text and styles

--- src/components/FilterView.vue
+++ src/components/FilterView.vue
@@ -1,101 +1,106 @@
 <template>
     <div>
+        <div class="filterViewHeader">
+            Demographics Filter
+        </div>    
+        <div>
+        <div class="selectBlock">
+            <span class="ageLabel">Male Age</span>
+            <select id="MaleAgeSelect" v-model="selectedM" @change="selectedAgeBand">
+                    <option v-for="option in optionsM" v-bind:key="option.value" v-bind:value="option.value">
+                        {{ option.text }}
+                    </option>    
+            </select>
+        </div>
+        <div class="selectBlock">        
+            <span class="ageLabel">Female Age</span>
+            <select id="FemaleAgeSelect" v-model="selectedF" @change="selectedAgeBand">
+                    <option v-for="option in optionsF" v-bind:key="option.value" v-bind:value="option.value">
+                        {{ option.text }}
+                    </option>    
+            </select>
+        </div>
+        </div>
         <button id="clearSelection" @click="clearSelections">Clear</button>
-<div class="selectBlock">
-        <span class="ageLabel">Male Age</span>
-        <select id="MaleAgeSelect" v-model="selectedM" @change="selectedAgeBand">
-                <option v-for="option in optionsM" v-bind:key="option.value" v-bind:value="option.value">
-                    {{ option.text }}
-                </option>    
-        </select>
-</div>
-<div class="selectBlock">        
-        <span class="ageLabel">Female Age</span>
-        <select id="FemaleAgeSelect" v-model="selectedF" @change="selectedAgeBand">
-                <option v-for="option in optionsF" v-bind:key="option.value" v-bind:value="option.value">
-                    {{ option.text }}
-                </option>    
-        </select>
-</div>
+
     </div>       
 </template>
 
 <script>
 import * as d3 from "d3";
-import { EventBus } from '../main.js';
+import { EventBus } from "../main.js";
 
 export default {
-        name: 'select1',
-        props: {fileLocation: String},
-        data() {
-            return {
-            selectedM: '',
-            selectedF: '',
-            filePlace: this.fileLocation,    
-            optionsM: loadSelect('data/lookups/B01001_Male.csv'),
-            optionsF: loadSelect('data/lookups/B01001_Female.csv'),
-            selectedList: []
-            }
-        },
-        methods: {
-            selectedAgeBand: function(event) {
-                console.log(event.currentTarget.value);
-                let msg = {};
-                msg.meta = event.currentTarget.id.split('Age')[0];
-                msg.value = event.currentTarget.value;
-                msg.label = event.currentTarget.selectedOptions[0].label;
-                this.selectedList.push(msg);
-                EventBus.$emit('selectionChange', this.selectedList);
-
-            },
-            clearSelections: function(event) {
-                this.selectedList = [];
-                this.selectedM = '';
-                this.selectedF = '';
-                EventBus.$emit('selectionChange', this.selectedList);
-            }
-        }
-}
+  name: "select1",
+  props: { fileLocation: String },
+  data() {
+    return {
+      selectedM: "",
+      selectedF: "",
+      filePlace: this.fileLocation,
+      optionsM: loadSelect("data/lookups/B01001_Male.csv"),
+      optionsF: loadSelect("data/lookups/B01001_Female.csv"),
+      selectedList: []
+    };
+  },
+  methods: {
+    selectedAgeBand: function(event) {
+      console.log(event.currentTarget.value);
+      let msg = {};
+      msg.meta = event.currentTarget.id.split("Age")[0];
+      msg.value = event.currentTarget.value;
+      msg.label = event.currentTarget.selectedOptions[0].label;
+      this.selectedList.push(msg);
+      EventBus.$emit("selectionChange", this.selectedList);
+    },
+    clearSelections: function(event) {
+      this.selectedList = [];
+      this.selectedM = "";
+      this.selectedF = "";
+      EventBus.$emit("selectionChange", this.selectedList);
+    }
+  }
+};
 
 function loadSelect(gender) {
-
-   let selectArray = [];
-   d3.csv(gender, function(d) {
-      return {
-        key: d.key,  
-        value: d.value,
-        selected: false
-      };
-    }).then(function(data) {
-        data.forEach(element => {
-            selectArray.push({text: element.value, value: element.key});    
-        });
-        
+  let selectArray = [];
+  d3.csv(gender, function(d) {
+    return {
+      key: d.key,
+      value: d.value,
+      selected: false
+    };
+  }).then(function(data) {
+    data.forEach(element => {
+      selectArray.push({ text: element.value, value: element.key });
     });
-    return selectArray;
-}        
-
-    
+  });
+  return selectArray;
+}
 </script>
 
 <style>
 select {
-    font-size: 14px;
-    float: right;
+  font-size: 14px;
+  float: right;
 }
 
 button {
-    font-size: 14px;
-    margin-right: 10px;
-    border-radius: 5px;
+  font-size: 14px;
+  margin-right: 10px;
+  border-radius: 5px;
 }
 
 .ageLabel {
-    padding: 5px;
-    font-size: 14px;
+  padding: 5px;
+  font-size: 14px;
 }
 
 .selectBlock {
-    padding: 10px;
+  padding: 10px;
+}
+
+.filterViewHeader {
+    text-align: center;
 }
 </style>
