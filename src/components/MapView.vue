e251c27d8dcdbcc60cd74838e0e9da5cee180187
John Hellier <john@Johns-MacBook-Pro-2.local>
2018-10-13 11:48:11 -0600

    Added Twitter feeds

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -20,20 +20,17 @@ import bcData from "../../public/data/clean/BernallioCensusBlocks_Joined.json";
 
 /* eslint-disable */
 
-
-
 let log = true;
 
 export default {
   name: "map-view",
-
-  props: {},
 
   data() {
     return {
       bcData,
       fbCheckins: [],
       busTypes: {},
+      twitterFeeds: [],
       toolTipDiv: {},
       mainMap: {},
       mainLayer: {},
@@ -47,127 +44,27 @@ export default {
   },
 
   mounted() {
-    var that = this;
 
+    // Event Bus is used to handle events between components
+    this.addEventBusHandlers();
 
-// Define the div for the tooltip
-that.div = d3.select("body").append("div")	
-    .attr("class", "toolTip")				
-    .style("opacity", 0);
+    // Builds the main map with the Bernallio Census Blocks
+    this.buildMap();
 
-    that.totalTargetPopulation = that.getTargetPopulation('B01001-HD01_VD06');
-   
+    // Use this constuct for summary BC census data.
+    //this.totalTargetPopulation = this.getTargetPopulation('B01001-HD01_VD06');
 
-    EventBus.$on("selectionChange", msg => {
-      if (log) {
-        console.log(" This is ", msg);
-      }
-      that.selectedAges = msg;
+    // Once the map is placed adjust the dimensions
+    this.adjustDimensions();
 
-      if (msg.length != 0)
-        that.clearLayer(false);
-      else 
-        that.clearLayer(true);  
-     });
+    // Add the facebook checkin data as D3 circle elements
+    this.getFacebookCheckins();
 
-    that.cWidth = document.getElementsByTagName("html")[0].clientWidth * 0.8;
-    that.cHeight = document.getElementsByTagName("html")[0].clientHeight * 0.8;
-
-    // Beware putting this after the tileLayer will give you a mini map.
-    document.getElementById("world").style.height = that.cHeight + "px";
-    document.getElementById("world").style.width = that.cWidth + "px";
-
-    that.mainMap = L.map("world").setView([35.1740721, -106.594435], 12);
-
-    L.tileLayer(
-      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-      {
-        attribution:
-          'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        minZoom: 2,
-        id: "mapbox.light",
-        accessToken: this.$mapboxAccessToken
-      }
-    ).addTo(that.mainMap);
-
-    that.mainMap.on("zoom", that.adjustDimensions);
-    that.mainMap.on("moveend", that.adjustPan);
-
-    let popup = L.popup();
-
-    that.mainLayer = L.geoJSON(bcData.features, {
-      // style: {
-      //   color: 'g#8cc48b'
-      // },
-      
-      onEachFeature: function(feature, layer) {
-        layer.setStyle({color: '#8cc48b' , fillOpacity: 0.2})          
-        layer.on("mouseover", function(e) {
-          // let target = e.target;
-          // let count = 0;
-          // let popupText = '';
-          // that.selectedAges.forEach(function(element) {
-          //   count += +e.sourceTarget.feature.properties[element.value];
-          //   popupText += '<div>' + element.meta 
-          //                   + ' ' + element.label 
-          //                   + ' ' + +e.sourceTarget.feature.properties[element.value]
-          //                   + '</div>';
-          // })
-          // let popupHeader = "<div class='popupHeader'>Total Count For Section: " + count + '</div>'; 
-          // // popup.setLatLng(e.latlng)
-          //     .setContent("<div class='popupBody'>" + popupHeader + popupText + '</div>')
-          //     .openOn(that.mainMap);
-        });
-        layer.on("mouseout", function(e) {
-          // mainLayer.resetStyle(this);
-        });
-        layer.on('click', function(e) {
-          let target = e.target;
-          let count = 0;
-          let popupText = '';
-          that.selectedAges.forEach(function(element) {
-            count += +e.sourceTarget.feature.properties[element.value];
-            popupText += '<div>' + element.meta 
-                            + ' ' + element.label 
-                            + ' ' + +e.sourceTarget.feature.properties[element.value]
-                            + '</div>';
-          })
-          let popupHeader = "<div class='popupHeader'>Total Count For Section: " + count + '</div>'; 
-         
-          
-          popup.setLatLng(e.latlng)
-              .setContent("<div class='popupBody'>" + popupHeader + popupText + '</div>')
-              .openOn(that.mainMap);
-
-        })
-      }.bind(this)
-    }).addTo(that.mainMap);
-
-    //let latLonPoint = new L.LatLng(40.7127837, -74.0059413);
-
-    L.svg().addTo(that.mainMap);
-
-    that.gMap = d3
-      .select("#world")
-      .select("svg")
-      .append("g");
-
-    that.adjustDimensions();
-    that.getFacebookCheckins();
-
-
-    // that.gMap.append('circle')
-    //   .attr('r',50)
-    //   .attr('cx',200)
-    //   .attr('cy',200)
-    //   .attr("pointer-events","visible")
-    //   .on('click', function(d) {
-    //     console.log('This is ', d);
-    //   })
+    // Get the Twitter feeds
+    this.getTwitterFeeds();
 
     // Register an event listener when the Vue component is ready
-    window.addEventListener("resize", that.onResize);
+    window.addEventListener("resize", this.onResize);
   },
 
   beforeDestroy() {
@@ -176,10 +73,127 @@ that.div = d3.select("body").append("div")
 
   methods: {
 
-    getColor(n) {
-        var colors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
-        return colors[n % colors.length];
+    buildMap() {
+      let that = this;
+      that.cWidth = document.getElementsByTagName("html")[0].clientWidth * 0.8;
+      that.cHeight =
+        document.getElementsByTagName("html")[0].clientHeight * 0.8;
+
+      // Beware putting this after the tileLayer declaration. It will give you a mini map.
+      document.getElementById("world").style.height = that.cHeight + "px";
+      document.getElementById("world").style.width = that.cWidth + "px";
+
+      that.mainMap = L.map("world").setView([35.1740721, -106.594435], 12);
+
+      L.tileLayer(
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+        {
+          attribution:
+            'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+          maxZoom: 18,
+          minZoom: 2,
+          id: "mapbox.light",
+          accessToken: this.$mapboxAccessToken
+        }
+      ).addTo(that.mainMap);
+
+      that.mainMap.on("zoom", that.adjustDimensions);
+      that.mainMap.on("moveend", that.adjustPan);
+
+      let popup = L.popup();
+
+      that.mainLayer = L.geoJSON(bcData.features, {
+
+        onEachFeature: function(feature, layer) {
+
+          layer.setStyle({ color: "#8cc48b", fillOpacity: 0.2 });
+
+          layer.on("click", function(e) {
+            let target = e.target;
+            let count = 0;
+            let popupText = "";
+            that.selectedAges.forEach(function(element) {
+              count += +e.sourceTarget.feature.properties[element.value];
+              popupText +=
+                "<div>" +
+                element.meta +
+                " " +
+                element.label +
+                " " +
+                +e.sourceTarget.feature.properties[element.value] +
+                "</div>";
+            });
+            let popupHeader =
+              "<div class='popupHeader'>Total Count For Section: " +
+              count +
+              "</div>";
+
+            popup
+              .setLatLng(e.latlng)
+              .setContent(
+                "<div class='popupBody'>" + popupHeader + popupText + "</div>"
+              )
+              .openOn(that.mainMap);
+          });
+
+        }.bind(this)
+
+      }).addTo(that.mainMap);
+
+      L.svg().addTo(that.mainMap);
+
+      // Add a D3 SVG and group for later SVG element placement
+      // This creates something like a layer. In order to have mouse interaction on
+      // be sure to add .attr("pointer-events","visible") to any appended element
+      that.gMap = d3
+        .select("#world")
+        .select("svg")
+        .append("g");
     },
+    
+    // This is a div based tooltip for the Facebook checkins where each
+    // checkin location is rendered as a D3 circle element
+    addTooltipDiv() {
+      // Define the div for the tooltip
+      this.div = d3
+        .select("body")
+        .append("div")
+        .attr("class", "toolTip")
+        .style("opacity", 0);
+    },
+
+    // This is used to color the circles of the Facebook checkins
+    // It is not the best way to cover a large number of categories. In this case
+    // the bulk of the data is confined to the first 15 or so categories, so this
+    // largely works. Another scheme might be needed for a more distributed data set    
+    getColor(n) {
+      var colors = [
+        "#3366cc",
+        "#dc3912",
+        "#ff9900",
+        "#109618",
+        "#990099",
+        "#0099c6",
+        "#dd4477",
+        "#66aa00",
+        "#b82e2e",
+        "#316395",
+        "#994499",
+        "#22aa99",
+        "#aaaa11",
+        "#6633cc",
+        "#e67300",
+        "#8b0707",
+        "#651067",
+        "#329262",
+        "#5574a6",
+        "#3b3eac"
+      ];
+      return colors[n % colors.length];
+    },
+
+    // Get the facebook checkins and generates a set of D3 circle elements
+    // based on the lat/lon data for each checkin location
     getFacebookCheckins() {
       let that = this;
 
@@ -188,56 +202,104 @@ that.div = d3.select("body").append("div")
           name: d.column1
         };
       }).then(function(data) {
-        data.forEach(function(element,index) {that.busTypes[element.name] = index})
-      })
+        data.forEach(function(element, index) {
+          that.busTypes[element.name] = index;
+        });
+      });
 
       d3.csv("data/clean/FacebookPlaces_ABQ.csv", function(d) {
-          return {
-            place: d.place,
-            bus_type: d.bus_type,
-            checkins: d.checkins,
-            lat: d.lat,
-            lon: d.lon,
-            LatLng: new L.LatLng(d.lat,d.lon)
-          };
-        }).then(function(data) {
-          that.fbCheckins = data;
-          //console.log(that.fbCheckins);  
-           that.gMap.selectAll('circle')
-            .data(that.fbCheckins.filter(element => element.checkins > 5000))
-            .enter()
-            .append('circle')
-            .attr("pointer-events","visible")
-            .attr('r',5)
-            .attr('fill',function(d,i) { return that.getColor(that.busTypes[d.bus_type])})
-            .attr("cx", d => that.mainMap.latLngToLayerPoint(d.LatLng).x)
-            .attr("cy", d => that.mainMap.latLngToLayerPoint(d.LatLng).y)
-        .on('mouseover', function(d) {
-            d3.selectAll('.toolTip')
-                // .transition()		
-                // .duration(200)		
-                .style("opacity", .9);		
-            d3.selectAll('.toolTip').html(d.place + '<br>' + d.bus_type)	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");
-          
-          
-            d3.select(this).style('cursor','pointer')})
-        .on('mouseout', function(d) { 
-
-            d3.select(this).style('cursor','default')
-            d3.select('.toolTip')
-                // .transition()		
-                // .duration(500)		
-                .style("opacity", 0);	
+        return {
+          place: d.place,
+          bus_type: d.bus_type,
+          checkins: d.checkins,
+          lat: d.lat,
+          lon: d.lon,
+          LatLng: new L.LatLng(d.lat, d.lon)
+        };
+      }).then(function(data) {
+        that.fbCheckins = data;
+        //console.log(that.fbCheckins);
+        that.gMap
+          .selectAll("circle")
+          .data(that.fbCheckins.filter(element => element.checkins > 5000))
+          .enter()
+          .append("circle")
+          .attr("pointer-events", "visible")
+          .attr("r", 5)
+          .attr("fill", function(d, i) {
+            return that.getColor(that.busTypes[d.bus_type]);
           })
-        });
+          .attr("cx", d => that.mainMap.latLngToLayerPoint(d.LatLng).x)
+          .attr("cy", d => that.mainMap.latLngToLayerPoint(d.LatLng).y)
+          .on("mouseover", function(d) {
+            d3.selectAll(".toolTip")
+              .style("opacity", 0.9);
+            d3.selectAll(".toolTip")
+              .html(d.place + "<br>" + d.bus_type)
+              .style("left", d3.event.pageX + "px")
+              .style("top", d3.event.pageY - 28 + "px");
+
+            d3.select(this).style("cursor", "pointer");
+          })
+          .on("mouseout", function(d) {
+            d3.select(this).style("cursor", "default");
+            d3.select(".toolTip")
+              .style("opacity", 0);
+          });
+      });
+     // Adds div based tooltips for the facebook checkin elements
+     that.addTooltipDiv();
 
     },
+
+    getTwitterFeeds() {
+      let that = this;
+
+      d3.csv("data/clean/Twitter_ABQ_2014_10_30.csv", function(d) {
+        return {
+          tweet: d.tweet,
+          username: d.username,
+          time: d.time,
+          lat: d.lat,
+          lon: d.lon,
+          LatLng: new L.LatLng(d.lat, d.lon)
+        };
+      }).then(function(data) {
+        that.twitterFeeds = data;
+        //console.log(that.fbCheckins);
+        that.gMap
+          .selectAll("rect")
+          //.data(that.twitterFeeds.filter(element => element.checkins > 5000))
+          .data(that.twitterFeeds)
+          .enter()
+          .append("rect")
+          .attr("pointer-events", "visible")
+          .attr('x', d => that.mainMap.latLngToLayerPoint(d.LatLng).x)
+          .attr('y', d => that.mainMap.latLngToLayerPoint(d.LatLng).y)
+          .attr('height',5)
+          .attr('width',5)
+          .on("mouseover", function(d) {
+            d3.selectAll(".toolTip")
+              .style("opacity", 0.9);
+            d3.selectAll(".toolTip")
+              .html(d.tweet + "<br>" + d.username + "<br>" + d.time)
+              .style("left", d3.event.pageX + "px")
+              .style("top", d3.event.pageY - 28 + "px");
+
+            d3.select(this).style("cursor", "pointer");
+          })
+          .on("mouseout", function(d) {
+            d3.select(this).style("cursor", "default");
+            d3.select(".toolTip")
+              .style("opacity", 0);
+          });
+      })          
+    },
+
     onResize(event) {
       this.adjustDimensions();
     },
-    adjustPan() {},
+
     adjustDimensions() {
       this.cWidth = document.getElementsByTagName("html")[0].clientWidth * 0.7;
       this.cHeight =
@@ -250,15 +312,24 @@ that.div = d3.select("body").append("div")
         .attr("height", this.cHeight)
         .attr("width", this.cWidth);
     },
+
+    // Used for get the pixel location given a set of lat long points
+    // This returns the Lat pixel location or the x
     getPXFromLat(latLng) {
       let latLonPoint = new L.LatLng(latLng.lat, latLng.lng);
       return this.mainMap.latLngToLayerPoint(latLonPoint).x;
     },
+
+    // Used for get the pixel location given a set of lat long points
+    // This returns the Long pixel location or the y
     getPXFromLng(latLng) {
       let latLonPoint = new L.LatLng(latLng.lat, latLng.lng);
       return this.mainMap.latLngToLayerPoint(latLonPoint).y;
     },
 
+    // This gets the total population from the BC Census data
+    // The key required is of the form: B01001-HD01_VD01 depending on
+    // which population band or other demographic band
     getTotalPopulation(targetKey) {
       let features = this.bcData.features;
       this.totalPopulation = 0;
@@ -266,10 +337,12 @@ that.div = d3.select("body").append("div")
         let amount = element.properties[targetKey];
         this.totalPopulation += +amount;
       });
-     // console.log(this.totalPopulation);
-      
+      // console.log(this.totalPopulation);
     },
 
+    // This gets the target population from the BC Census data
+    // The key required is of the form: B01001-HD01_VD01 depending on
+    // which population band or other demographic band
     getTargetPopulation(targetKey) {
       let features = this.bcData.features;
       let total = 0;
@@ -280,25 +353,37 @@ that.div = d3.select("body").append("div")
       //console.log(total);
       return total;
     },
-    
+
+    // This resets the filter and the map for BC Census data
     clearLayer(clear) {
       let that = this;
       this.mainLayer.eachLayer(function(featureLayer) {
-        featureLayer.setStyle(function(el,feature) {
-          if (clear) {
-            return {color: '#8cc48b', fillOpacity: 0.2};
-            //that.mainLayer.resetStyle();
-          }
-          let count = 0;
-          that.selectedAges.forEach(function(element) {
-            count += +feature.feature.properties[element.value];;
-            return count;
-          })
-          var opacity = count/(that.selectedAges.length * 200);
-          return {color: 'rgba(0,215,0,0.8', "fillOpacity": opacity}
-          
-        }(that, featureLayer));
-      })
+        featureLayer.setStyle(
+          (function(el, feature) {
+            if (clear) {
+              return { color: "#8cc48b", fillOpacity: 0.2 };
+            }
+            let count = 0;
+            that.selectedAges.forEach(function(element) {
+              count += +feature.feature.properties[element.value];
+              return count;
+            });
+            var opacity = count / (that.selectedAges.length * 200);
+            return { color: "rgba(0,215,0,0.8", fillOpacity: opacity };
+          })(that, featureLayer)
+        );
+      });
+    },
+
+    // This is a utility function that will declare handlers any EventBus events
+    // from other components. All your EventBus handlers should be declared in here.
+    addEventBusHandlers() {
+      EventBus.$on("selectionChange", msg => {
+        this.selectedAges = msg;
+
+        if (msg.length != 0) this.clearLayer(false);
+        else this.clearLayer(true);
+      });
     }
   }
 };
@@ -311,7 +396,7 @@ that.div = d3.select("body").append("div")
   margin-left: 10px;
 }
 
- .selectedAges {
+.selectedAges {
   z-index: 1000;
   position: absolute;
   right: 30px;
@@ -331,20 +416,18 @@ that.div = d3.select("body").append("div")
   font-size: 12px;
 }
 
-div.toolTip {	
-    position: absolute;			
-    text-align: center;			
-    width: 200px;					
-    height: 50px;					
-    padding: 2px;				
-    font: 12px sans-serif;		
-    background: rgb(218, 220, 223);	
-    border: 1px;
-    border-style: solid;		
-    border-radius: 8px;			
-    pointer-events: none;	
-    z-index: 2000;
-
+div.toolTip {
+  position: absolute;
+  text-align: center;
+  width: 200px;
+  height: 50px;
+  padding: 2px;
+  font: 12px sans-serif;
+  background: rgb(218, 220, 223);
+  border: 1px;
+  border-style: solid;
+  border-radius: 8px;
+  pointer-events: none;
+  z-index: 2000;
 }
-
 </style>
